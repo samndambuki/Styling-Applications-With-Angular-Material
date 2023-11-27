055f1a9b048baa7559f371c1ca2646f79915f97e
samndambuki <sam.ndambuki08@gmail.com>
2023-11-27 09:52:02 +0300

    flexbox demo

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -7,9 +7,10 @@ import { MaterialModule } from '../shared/material.module';
 
 import { FormsModule } from '@angular/forms';
 import { FlexboxComponent } from './flexbox/flexbox.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [ButtonsComponent, FlexboxComponent],
-  imports: [MaterialModule, DemoRoutingModule, FormsModule],
+  imports: [MaterialModule, DemoRoutingModule, FormsModule, FlexLayoutModule],
 })
 export class DemoModule {}
