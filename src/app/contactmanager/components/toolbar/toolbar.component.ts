43a44555dfc5a4466f0200d08033ebce47047a6f
samndambuki <sam.ndambuki08@gmail.com>
2023-11-27 14:37:12 +0300

    toggle button done

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent {
-
+  @Output() toggleSidenav = new EventEmitter<void>();
 }
